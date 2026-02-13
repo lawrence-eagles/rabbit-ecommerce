4c17c84eb29e329bd8f3deeb73c390652bed7662
Lawrence <code.matt.eagles@gmail.com>
2026-02-13 14:59:30 +0100

    styled cart drawer

--- frontend/src/components/Layout/CartDrawer.jsx
+++ frontend/src/components/Layout/CartDrawer.jsx
@@ -11,6 +11,21 @@ const CartDrawer = ({ drawerOpen, toggleCartDrawer }) => {
           <IoMdClose className="h-6 w-6 text-gray-600" />
         </button>
       </div>
+      {/* cart content with scrollable area */}
+      <div className="flex-grow p-4 overflow-y-auto">
+        <h2 className="text-xl font-semibold mb-4">Your Cart</h2>
+        {/* cart content */}
+      </div>
+
+      {/* checkout button fixed at the bottom */}
+      <div className="p-4 bg-white sticky botton-0">
+        <button className="w-full bg-black text-white py-3 rounded-lg font-semibold hover:bg-gray-800 transition">
+          Checkout
+        </button>
+        <p className="text-sm tracking-tighter text-gray-500 mt-2 text-center">
+          Shipping, taxes, and discount codes calculated at checkout.
+        </p>
+      </div>
     </div>
   );
 };
