078aa15297a8fa5bc7c134583cd7563e1ea877bc
Lawrence <code.matt.eagles@gmail.com>
2026-02-21 20:29:40 +0100

    completed homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Hero from "../components/Layout/Hero";
 import FeaturedCollection from "../components/Products/FeaturedCollection";
+import FeaturesSection from "../components/Products/FeaturesSection";
 import GenderCollectionSection from "../components/Products/GenderCollectionSection";
 import NewArrivals from "../components/Products/NewArrivals";
 import ProductDetails from "../components/Products/ProductDetails.";
@@ -76,6 +77,9 @@ const Home = () => {
 
       {/* Featured Collection */}
       <FeaturedCollection />
+
+      {/* Features Section */}
+      <FeaturesSection />
     </div>
   );
 };
