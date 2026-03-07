952b60f206c3faf0c71ca9ab19755f33bd51ce0b
Lawrence <code.matt.eagles@gmail.com>
2026-03-07 14:11:56 +0100

    implementd authMiddleware and finished user routes implementation

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
+const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
@@ -104,6 +105,13 @@ router.post("/login", async (req, res) => {
     console.log(error);
     res.status(500).send("Server Error");
   }
+});
+
+// @route GET /api/users/profile
+// @desc Get logged-in user profile (protected route)
+// @access Private
+router.get("/profile", protect, async (req, res) => {
+  res.json(req.user);
 });
 
 module.exports = router;
